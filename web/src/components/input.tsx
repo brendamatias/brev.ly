d3731aa49dd7d79985000b9bf3bb746f6d2c5a54
Brenda Matias <brendamatias.sobral@gmail.com>
2026-03-07 22:29:23 -0300

    feat: add input component

---
+++ web/src/components/input.tsx
@@ -0,0 +1,56 @@
+import { useId, type ComponentProps } from "react";
+import { Icon } from "./icon";
+import { cn } from "@/utils/tailwind";
+
+type InputProps = ComponentProps<"input"> & {
+  label: string;
+  error?: string;
+};
+
+export function Input({ id, label, error, className, ...props }: InputProps) {
+  const generatedId = useId();
+  const inputId = id ?? generatedId;
+  const errorId = `${inputId}-error`;
+
+  return (
+    <div className="group flex flex-col gap-2">
+      <label
+        htmlFor={inputId}
+        className={cn(
+          "text-xs uppercase focus-within:font-bold",
+          error
+            ? "text-feedback-danger font-bold"
+            : "text-gray-500 group-focus-within:text-blue-base group-focus-within:font-bold"
+        )}
+      >
+        {label}
+      </label>
+
+      <input
+        id={inputId}
+        {...props}
+        aria-invalid={!!error}
+        aria-describedby={error ? errorId : undefined}
+        className={cn(
+          "caret-blue-base h-12 rounded-lg border px-4 py-2 text-md font-normal text-gray-600 outline-none",
+          "focus:border-[1.5px]",
+          error
+            ? "border-[1.5px] border-feedback-danger"
+            : "border-gray-300 focus:border-blue-base",
+          className
+        )}
+      />
+
+      {error && (
+        <div id={errorId} role="alert" className="flex items-center gap-2">
+          <Icon
+            name="warning"
+            className="text-feedback-danger"
+            aria-hidden="true"
+          />
+          <span className="text-sm text-gray-500">{error}</span>
+        </div>
+      )}
+    </div>
+  );
+}
